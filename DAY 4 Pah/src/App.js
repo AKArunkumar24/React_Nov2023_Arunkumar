1693b20b54ad18086b808b4d2349448de387f2c5
AKArunkumar24 <150216196+AKArunkumar24@users.noreply.github.com>
2023-11-17 16:17:35 +0530

    Day4 Pah

---
+++ DAY 4 Pah/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginDialog from './components/LoginDialog.js';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginDialog/>
+    </div>
+  );
+}
+
+export default App;
